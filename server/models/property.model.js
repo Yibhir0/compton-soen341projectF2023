1d260191c6c75fbcfb86b241bb9da810aac824b4
Steven Di Stefano <107154727+stevendis@users.noreply.github.com>
2023-10-15 19:53:03 -0400

    Updated and added fields for property.

--- server/models/property.model.js
+++ server/models/property.model.js
@@ -4,16 +4,47 @@ const Schema = mongoose.Schema;
 
 /* Creating a new schema for the property model. */
 const propertySchema = new Schema({
-  name: {
-    type: String,
+  brokerID: {
+    type: Number,
     required: true,
   },
+
   address: {
     type: String,
     required: true,
   },
 
+  city:{
+    type: String,
+    required: true,
+  },
+  
+  postalCode:{
+    type: String,
+    required: true,
+  },
+
   propertyType: {
+    type: String,
+    required: true,
+  },
+
+  price:{
+    type: Number,
+    required: true,
+  },
+
+  numberOfBedrooms:{
+    type: Number,
+    required: true,
+  },
+
+  numberOfBathrooms:{
+    type: Number,
+    required: true,
+  },
+
+  amenities:{
     type: String,
     required: true,
   },
