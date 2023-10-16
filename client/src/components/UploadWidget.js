8a0752a71b3ad22650ba1ad1b9c0629f48bc8eee
Steven Di Stefano <107154727+stevendis@users.noreply.github.com>
2023-10-16 16:56:54 -0400

    added imageUrl

--- client/src/components/UploadWidget.js
+++ client/src/components/UploadWidget.js
@@ -1,24 +1,38 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 const UploadWidget = () =>{
     const cloudinaryRef = useRef();
     const widgetRef = useRef();
+
+    //const [uploadedImageUrl, setUploadedImageUrl] = useState(null);
+
     useEffect(()=> {
         cloudinaryRef.current = window.cloudinary;
         widgetRef.current = cloudinaryRef.current.createUploadWidget({
             cloudName:'dbhsjm5a2',
             uploadPreset: 'zm8dul6u'
         }, function(error,result){
-            console.log(result);
+            if (!error && result && result.event === 'success') {
+                // provides url to uploaded image
+                const imageUrl = result.info.secure_url;
+                //setUploadedImageUrl(imageUrl);
+                
+                console.log("Image URL:", imageUrl);
+            }
         });
     }, [])
 
-    return(
-
-    <button onClick={() => widgetRef.current.open()}>
-        Upload
-    </button>
-    )
-}
+    return (
+        <div>
+          <button onClick={() => widgetRef.current.open()}>Upload</button>
+          {/* {uploadedImageUrl && (
+            <div>
+              <p>Uploaded Image URL:</p>
+              <img src={uploadedImageUrl} alt="Uploaded" />
+            </div>
+          )} */}
+        </div>
+      );
+    };
 
 export default UploadWidget
