da85e0974dcf8a41b78f611178db9066aee86f5b
Steven Di Stefano <107154727+stevendis@users.noreply.github.com>
2023-10-21 15:43:59 -0400

    Added a way to fetch a single property by id

--- server/controllers/property.controller.js
+++ server/controllers/property.controller.js
@@ -32,9 +32,32 @@ const addProperty = async (req, res) => {
   }
 };
 
+/**
+ * Fetches a single property listing by its id (per request param).
+ */
+
+const getProperty = async(req, res) => {
+  try{
+    const {id} = req.params;
+    const property = await Property.findById(id);
+    if(!property){
+      return res.status(404).json({messahe: "Property not found with id " + id})
+    }
+    res.status(200).json(property);
+  } catch(err){
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const updateProperty = async (req, res) =>{
+
+}
+
 // to Do (delete,edit..etc)
 
 module.exports = {
- getProperties,
- addProperty,
-};
+  getProperties,
+  addProperty,
+  getProperty,
+  updateProperty,
+ };
--- server/routes/property.route.js
+++ server/routes/property.route.js
@@ -3,6 +3,8 @@ const express = require("express");
 const {
   getProperties,
   addProperty,
+  getProperty,
+  updateProperty,
 } = require("../controllers/property.controller");
 
 const router = express.Router();
@@ -11,5 +13,9 @@ const router = express.Router();
 router.get("/properties", getProperties);
 /* Creating a route for the post request. */
 router.post("/property", addProperty);
+/* Creating a route for the get request. */
+router.get("/properties/:id", getProperty);
+/* Creating a route for the put request. */
+router.put("/properties/:id", updateProperty);
 
 module.exports = router;
