75f7229b551cb41f2b76565bb7b209a6aa1a0b56
Yassine_Ibhir <yibhir101@gmail.com>
2023-10-25 15:57:33 -0400

    fixed conflict and refactored

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
-import { BrowserRouter } from 'react-router-dom';
 
 
 import Home from "./pages/home/home";
--- client/src/components/menu/navigationBar.js
+++ client/src/components/menu/navigationBar.js
@@ -1,40 +1,5 @@
-<<<<<<< HEAD
-import { NavLink } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './navigationBar.css';
-const Navbar=()=>{
-    return (    
-            <nav className="navbar"> 
-                        <div className="container">                                                  
-                                <div className="nav-elements">
-                                        <ul>
-                                            <li>
-                                            <img className="logo-realestate" alt="Logo Real Estate" src={require('../../assets/images/placeholder_logo.png')}/>
-                                            </li>
-                                                    <li>
-                                                    <NavLink to="/">Home</NavLink>
-                                                    </li>
-                                                    <li> 
-                                                    <NavLink to="/login">Login</NavLink>
-                                                    </li>
-                                                    <li>
-                                                    <NavLink to="/about">About</NavLink>
-                                                    </li>
-                                                    <li>
-                                                    <NavLink to="/demo">Demo</NavLink>
-                                                    </li>
-                                                    <li>
-                                                    <NavLink to="/properties">Property Listings</NavLink>
-                                                    </li>
-                                        </ul>      
-                                </div>     
-                         </div>        
-            </nav>
-    )
-=======
 import { Link, useNavigate } from 'react-router-dom';
 import './navigationBar.css';
->>>>>>> e8629ce40410ef1730a7b0ea93836ac944b0fc95
 
 const Navbar = () => {
   const isBrokerSignedIn = !!localStorage.getItem('token');
@@ -50,47 +15,57 @@ const Navbar = () => {
   // Available navbar links for both logged-in and non-logged-in users.
   const navigation = (
     <>
-      <div>
+      <li>
         <Link to="/" className='navItem'>Home</Link>
-      </div>
-      <div className='navItem'>
+      </li>
+      <li className='navItem'>
         <Link to="/about" className='navItem'>About</Link>
-      </div>
+      </li>
     </>
   );
 
   // Available navbar links only if isBrokerSignedIn is true
   const brokerNavigation = isBrokerSignedIn ? (
     <>
-      <div className='navItem'>
+      <li> 
         <Link to="/create" className='navItem'>Create Property Listing</Link>
-      </div>
-      <div className='navItem'>
+      </li>
+      <li>
         <Link to="/properties" className='navItem'>My Properties</Link>
-      </div>
-      <div>
+      </li>
+      <li>
         <Link to="/" onClick={handleSignout} className='navItem'>Sign out</Link>
-      </div>
+      </li>
 
     </>
   ) : (
     //Available navbar links only if isBrokerSignedIn is false
     <>
-      <div>
+      <li>
         <Link to="/login" className='navItem'>Login</Link>
-      </div>
-      <div>
+      </li>
+      <li>
         <Link to="/register" className='navItem'>Register</Link>
-      </div>
+      </li>
     </>
   );
 
   return (
-    <div className='navBar'>
-      {navigation}
-      {brokerNavigation}
-    </div>
+    
+      <nav className="navbar"> 
+                  <div className="container">                                                  
+                          <div className="nav-elements">
+                                  <ul>
+                                      <li>
+                                      <img className="logo-realestate" alt="Logo Real Estate" src={require('../../assets/images/placeholder_logo.png')}/>
+                                      </li>
+                                      {navigation}
+                                      {brokerNavigation}
+                                              
+                                  </ul>      
+                          </div>     
+                   </div>        
+      </nav>
   );
 }
-
 export default Navbar;
