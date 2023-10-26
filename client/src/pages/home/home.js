fbad76ad3a3c5bcae38516545d270e0333bdd7df
Michael Iadisernia <michaelaiadisernia@gmail.com>
2023-10-26 01:44:22 -0400

    added search bar to landing page and began recentl ly visited feature

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -2,14 +2,23 @@ import React from 'react';
 import  { Box } from '@mui/material';
 import NavBar from "../../components/menu/navigationBar"
 import './home.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+
+import FilterSearch from '../../components/form/filterSearch';
 
 function Home()  {
   console.log("home");
   return (
+    
     <div className="home-page">
                          <div>
                           <NavBar/>
                           </div>
+                          
       <div className="mainContent">                   
         <div className="window-1">
                         <div className="background">
@@ -27,10 +36,49 @@ function Home()  {
                       <div className="label">
           
                           <p className="label-welcome">Welcome to Compton Real Estate</p>
-                          <p className="label-subtext">In oculis quidem se esse admonere interesse enim maxime placeat, facere possimus, omnis. Et quidem faciunt, ut
-                          labore et accurate disserendum et harum quidem exercitus quid.</p>
-                        
+                          <p className="label-subtext">Find your next home with us.</p>
                       </div>
+
+                      <div className="searchbar-container">
+                          <div className="searchbar">
+                            <FilterSearch/>
+                          </div>
+                      </div>        
+
+                      <div className="searchProperty"></div> 
+                      <div className="recentlyViewedListings">
+                        <p className="recentlyViewedText">Recently Viewed</p>
+                      </div>       
+                              <div className="recent-listings">
+                              <Swiper  slides-per-view="3" spaceBetween={30} navigation={true} modules={[Navigation]} className="mySwiper">
+
+                                        <SwiperSlide>
+                                           <div className="recentproperty-card">
+                                              <Box sx={{height: 400, width: 325, border: '1px dashed'}}></Box> 
+                                                <div className="recentproperty-card header">
+      
+
+                                                </div>
+                                              
+                                             
+                                              <p>Slide 1</p>
+                                            </div>
+                                        </SwiperSlide>
+                                        <SwiperSlide>Slide 2</SwiperSlide>
+                                        <SwiperSlide>Slide 3</SwiperSlide>
+                                        <SwiperSlide>Slide 4</SwiperSlide>
+                                        
+                                        <swiper-navigation>
+                                          <swiper-button-prev></swiper-button-prev>
+                                          <swiper-button-next></swiper-button-next>
+                                        </swiper-navigation>
+
+                              </Swiper>
+
+                              </div>
+                                                        
+
+                         
         </div>  
       </div>                
     </div>
