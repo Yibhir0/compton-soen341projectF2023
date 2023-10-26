0c426a1afd51b67ab0ac9b15f0cb738b1a08518d
Yassine_Ibhir <yibhir101@gmail.com>
2023-10-26 14:22:34 -0400

    added brokers list

--- client/src/pages/user/userDetails.js
+++ client/src/pages/user/userDetails.js
@@ -1,14 +1,44 @@
 
 import React, {  } from 'react';
 import { useLocation} from "react-router-dom";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
 const UserDetail = () => {
   
-    const { state } = useLocation();
+    const location = useLocation();
+    const {state} = location;
     console.log(state);
-    console.log(state)
+
 
   return (
-    <div></div>
+    <div>
+        <h1 style={{textAlign:"center"}}>Broker Details</h1>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection:"column",
+            justifyContent: 'space-between',
+            alignItems:"center"
+          }}>
+         
+  
+         
+         <Avatar
+          alt=""
+          sx={{ width: 160, height: 160 }}
+        />
+        <h6 className="b-text-font">{state.email}</h6>
+        <h6 className="b-text-font">{state.accountType}</h6>
+      </Box>
+
+      <h4>Visits</h4>
+      
+
+      
+
+
+
+    </div>
   )
 }
 
