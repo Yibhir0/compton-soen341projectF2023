b6d45e4f818a1755d85a3317dcb87e46351dc66e
Steven Di Stefano <107154727+stevendis@users.noreply.github.com>
2023-11-14 21:02:57 -0500

    added autocomplete to edit property details

--- client/src/pages/create/create.js
+++ client/src/pages/create/create.js
@@ -3,59 +3,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import jwtDecode from 'jwt-decode';
 import { useNavigate } from "react-router-dom";
 import { Box } from "@mui/material";
-
-
-function initLocationAutocomplete(setCoordinates) {
-  const componentForm = [
-    'address',
-    'locality',
-    'postal_code'
-  ];
-
-  const getFormInputElement = (component) => document.getElementById(component + '-input');
-  const autocompleteInput = getFormInputElement('address');
-  const autocomplete = new window.google.maps.places.Autocomplete(autocompleteInput, {
-    fields: ["address_components", "geometry", "name"],
-    types: ["address"],
-  });
-
-  autocomplete.addListener('place_changed', function () {
-    const place = autocomplete.getPlace();
-    if (!place.geometry) {
-      window.alert('Location not found: \'' + place.name + '\'');
-      return;
-    }
-    setCoordinates({
-      latitude: place.geometry.location.lat(),
-      longitude: place.geometry.location.lng(),
-    });
-    fillInAddress(place);
-  });
-
-  function fillInAddress(place) {
-    const addressNameFormat = {
-      'street_number': 'short_name',
-      'route': 'long_name',
-      'locality': 'long_name',
-      'postal_code': 'short_name',
-    };
-    const getAddressComponent = function (type) {
-      for (const component of place.address_components) {
-        if (component.types[0] === type) {
-          return component[addressNameFormat[type]];
-        }
-      }
-      return '';
-    };
-    getFormInputElement('address').value = getAddressComponent('street_number') + ' ' + getAddressComponent('route');
-    for (const component of componentForm) {
-      if (component !== 'address') {
-        getFormInputElement(component).value = getAddressComponent(component);
-      }
-    }
-  }
-  
-}
+import {initLocationAutocomplete} from '../../components/autocomplete/autocomplete'
 
 /*This is the property creation pagee of the site.
 This would only be accesable by brokers to add a listing of a 
