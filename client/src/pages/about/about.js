1d7220081f8b230ca7b0d332def37407943809d5
Jared Latchman <latchmanjared@gmail.com>
2023-11-19 15:18:15 -0500

    Created the list of slides

--- client/src/pages/about/about.js
+++ client/src/pages/about/about.js
@@ -48,19 +48,44 @@ function About(props) {
 //   )
 
 
-const ABOUTPAGE = [
+var ABOUTPAGE = [
   {
       Head1: "Comptant Real Estate Company",
-      Head2: "The Compton Real Estate company is a student project\nAt Concordia University for the Fall 2023 SOEN341 Course",
+      Head2: "The Compton Real Estate company is a student project \n At Concordia University for the Fall 2023 SOEN341 Course",
       Head3: "The Goal of the project was to create a real estate web app designed to help users buy, rent and sell residential properties."
       
   },
   
   {
-    Head1: "Comptant Real Estate Company",
-    Head2: "The Compton Real Estate company is a student project\nAt Concordia University for the Fall 2023 SOEN341 Course",
-    Head3: "The Goal of the project was to create a real estate web app designed to help users buy, rent and sell residential properties."
-  }
+    Head1: "Dominic Riccio ",
+    Head2: " Fullstack DEV",
+    Head3: "Electrical Engineering Student"
+  },
+  {
+    Head1: "Saoud Messaoudi ",
+    Head2: "Scrum master",
+    Head3: "Software Engineering Student"
+  },
+  {
+    Head1: "Steven Di Stefano ",
+    Head2: "Backend DEV",
+    Head3: "Software Engineering Student"
+  },
+  {
+    Head1: "Jared Latchman ",
+    Head2: "Front-end DEV",
+    Head3: "Computer Engineering Student"
+  },
+  {
+    Head1: "Michael Iadisernia ",
+    Head2: "Front-end DEV",
+    Head3: "Computer Engineering Student"
+  },
+  {
+    Head1: "Yassine Ibhir ",
+    Head2: "Fullstack DEV",
+    Head3: "Software Engineering Student"
+  },
 ]
 
     // var items = [
@@ -81,8 +106,9 @@ const ABOUTPAGE = [
             {
 
               
-               // items.map( (item, i) => <Item key={i} item={item} /> )
-                students.map( (student, i) => <Item key={i} student={student}  />)
+              //  items.map( (item, i) => <Item key={i} item={item} /> )
+               // students.map( (student, i) => <Item key={i} student={student}  />)
+               ABOUTPAGE.map( (slide, i) => <Item key={i} slide={slide} /> )
 
 
              //  students.map((student, index) => (<li key={index}> {student}  </li>))
@@ -105,13 +131,21 @@ function Item(props)
             
             <p>{props.item.description}</p>  */}
 
-            <h1 className='property-header'>{props.student}
+            {/* <h1 className='property-header'>{props.student}
 
             </h1>
             <h2>{props.student}</h2>
 
             
-            <p>{props.student}</p> 
+            <p>{props.student}</p>  */}
+
+          <h1 className='property-header'>{props.slide.Head1}
+
+          </h1>
+          <h2>{props.slide.Head2}</h2>
+
+
+          <p>{props.slide.Head3}</p> 
 
 
 
