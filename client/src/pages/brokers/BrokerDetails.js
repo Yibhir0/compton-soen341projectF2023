6a6647c1f218723cf2165733c5b94a3f7e8aa2da
Steven Di Stefano <107154727+stevendis@users.noreply.github.com>
2023-11-26 01:06:34 -0500

    resized avatar, removed sold properties from active listings

--- client/src/pages/brokers/BrokerDetails.js
+++ client/src/pages/brokers/BrokerDetails.js
@@ -1,28 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-
+import PropertyList from "../../components/list/PropertyList";
 
 import brokerIcon from "../../assets/images/broker-icon.png"
 import {Row, Col, Card} from "reactstrap";
-import MyProperties from "../properties/my-properties";
 
 
 function BrokerDetails() {
     const [broker, setBroker] = useState();
+    const [properties, setProperties] = useState([]);
     const { id } = useParams();
 
 
     useEffect(() => {
         const fetchData = async () => {
-            const result = await fetch(
-                `${process.env.REACT_APP_BACKEND_URL}/user/brokers/${id}`
-            );
-            const data = await result.json();
-            setBroker(data);
+          const result = await fetch(
+            `${process.env.REACT_APP_BACKEND_URL}/user/brokers/${id}`
+          );
+          const data = await result.json();
+          setBroker(data);
         };
         fetchData();
-    }, [id]);
+    
+        const fetchProperties = async() =>{
+          const result = await fetch(
+            `${process.env.REACT_APP_BACKEND_URL}/getactivelistings/${id}`
+          );
+          const data = await result.json();
+          setProperties(data);
+        }
+        fetchProperties();
+      }, [id]);
 
 
     return (
@@ -31,20 +40,22 @@ function BrokerDetails() {
             <Card className={"card"}>
                 <Row>
                     <Col md="3" style={{borderRight:"1px solid #000", height:"300px"}}>
-            <img src={brokerIcon} style={{width:"80%", marginTop:"20px", marginLeft:"20px"}}/>
+            <img src={brokerIcon} style={{width:"50%", marginTop:"20px", marginLeft:"20px"}}/>
                     </Col>
                     <Col md="9" style={{paddingLeft:"50px", paddingTop:"20px"}}>
                 <p style={{fontWeight:"bold"}}>{broker?.firstName + " " + broker?.lastName}</p>
                         <p style={{marginTop:"-10px"}}>Real Estate Broker</p>
                         <br/>
                         <p>📧 {broker?.email}</p>
-                <p style={{fontStyle:"italic"}}>📞 {broker?.phoneNumber}</p>
-                <p>🪪 {broker?.licenseNumber}</p>
+                <p style={{fontStyle:"italic"}}>📞 {broker?.phoneNumber.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3')}</p>
+                <p>📄 {broker?.licenseNumber}</p>
                     </Col>
                 </Row>
             </Card>
-            <h2>Properties Listed</h2>
-            <MyProperties/>
+            <div>
+        <h1 style={{ textAlign: "center" }}>Active Listings</h1>
+      </div>
+      <PropertyList properties={properties} />
         </div>
 
     )
