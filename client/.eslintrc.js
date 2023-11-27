102be68b14d1c1a3d1b05ac7bf3ef6cd8b1890c7
Steven Di Stefano <107154727+stevendis@users.noreply.github.com>
2023-11-26 20:13:46 -0500

    added rules for remaining errors

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -37,6 +37,17 @@ module.exports = {
         "no-fallthrough": "off",
         "semi": "off",
         "quotes": "off",
-        "no-empty": "off"
+        "no-empty": "off",
+        "no-unused-vars": "off",
+        "no-cond-assign": "off",
+        "no-undef":"off",
+        "no-useless-escape":"off",
+        "no-misleading-character-class":"off",
+        "getter-return":"off",
+        "no-func-assign":"off",
+        "no-sparse-arrays":"off",
+        "no-redeclare":"off"
+
+
     }
 }
